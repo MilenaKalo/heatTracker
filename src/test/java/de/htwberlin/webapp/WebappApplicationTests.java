3bc5e505465430306cb7ccffe63472c7d871e6ee
milena <milenakalo@icloud.com>
2023-09-22 16:28:06 +0200

    first commit

---
+++ src/test/java/de/htwberlin/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
