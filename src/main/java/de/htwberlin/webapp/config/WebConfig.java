8b4190381c4e7bfeb84f132650e0679ed033c230
milena <milenakalo@icloud.com>
2023-12-06 15:34:23 +0100

    webconfig

---
+++ src/main/java/de/htwberlin/webapp/config/WebConfig.java
@@ -0,0 +1,23 @@
+package de.htwberlin.webapp.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@EnableWebMvc
+@CrossOrigin
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedMethods("*")
+                .allowedOrigins(
+                        "http://localhost:3000"
+                );
+    }
+}
